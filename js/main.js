38f30874d1976165feea7de0e86fd23505dd9b7d
Rafal Stepien <trafgraf@gmail.com>
2018-01-29 09:45:08 +0100

    Create very simple gross/net calculator

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var isDataEmpty = function(name, tax, net, gross) {
+    if (!name || !tax || !(net | gross)) return true;
+    return false;
+};
+
+document.getElementById('calcBtn').addEventListener('click', function (){
+   console.log("OK");
+   var name = document.getElementById('name').value;
+   var netValue = parseFloat(document.getElementById('net').value);
+   var grossValue = parseFloat(document.getElementById('gross').value);
+   var taxValue = document.getElementById('tax').value;
+
+   if (isDataEmpty(name, taxValue, netValue, grossValue)) {
+       alert("Provided data is not correct, you must provide at least name tax and net or gross value");
+       return false;
+   }
+
+
+   if (netValue) {
+       document.getElementById('gross').value = (netValue + parseFloat(netValue * parseFloat(taxValue / 100))).toPrecision(4);
+   }
+   else if (grossValue) {
+       document.getElementById('net').value = (grossValue / (1 + parseFloat(taxValue / 100))).toPrecision(4);
+   }
+});
